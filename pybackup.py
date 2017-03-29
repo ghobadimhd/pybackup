44dd8cc576b7ce568308c62b12b614bf61790f8f
mamad <ghobadimhd@gmail.com>
2017-03-30 01:46:48 +0430

    add configuration check

--- pybackup.py
+++ pybackup.py
@@ -7,7 +7,25 @@ import os
 def read_config(config_path):
     with open(config_path, 'r') as config_file:
         raw_config = config_file.read()
-    return yaml.load(raw_config)
+    # set default parameter
+    config = yaml.load(raw_config)
+    config['name'] = config.get('name', 'pybackup')
+    config['name_append_date'] = config.get('name_append_date', True)
+    config['date_format'] = config.get('date_format', "%Y-%m-%d")
+    config['archive_all'] = config.get('archive_all', True)
+    config['remove_raw_dir'] = config.get('remove_raw_dir', True)
+    config['compress_type'] = config.get('compress_type', 'bz2')
+    config['send_email'] = config.get('send_email', False)
+    config['base_dir'] = config.get('base_dir', os.path.abspath(os.curdir()))
+    config['rotate'] = config.get('rotate', 0)
+    # check configuration parameter
+    if 'datebases' in config and \
+        ('mysql_password' not in config or 'mysql_user' not in config):
+        print('error : mysql user of password not found')
+        print('abort')
+        exit(1)
+
+    return config
 
 def make_archive(name, files):
     try:
