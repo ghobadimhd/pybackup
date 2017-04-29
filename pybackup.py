c6e91ed097978d11a6b5c35989ab69aa284346d2
mamad <ghobadimhd@gmail.com>
2017-04-29 19:46:13 +0430

    support passwordless mysqldumping

--- pybackup.py
+++ pybackup.py
@@ -5,6 +5,8 @@ import yaml
 import subprocess as sp
 import os
 import shutil
+import getpass
+
 def read_config(config_path):
     with open(config_path, 'r') as config_file:
         raw_config = config_file.read()
@@ -20,11 +22,9 @@ def read_config(config_path):
     config['base_dir'] = config.get('base_dir', os.path.abspath(os.curdir))
     config['rotate'] = config.get('rotate', 0)
     # check configuration parameter
-    if 'datebases' in config and \
-        ('mysql_password' not in config or 'mysql_user' not in config):
-        print('error : mysql user of password not found')
-        print('abort')
-        exit(1)
+    if 'databases' in config:
+        config['mysql_user'] = config.get('mysql_user', getpass.getuser())
+        config['mysql_password'] = config.get('mysql_password', None)
 
     return config
 
@@ -35,13 +35,18 @@ def make_archive(name, files, compress_type='bz2'):
         for file in files:
             print('archiving : ' + file)
             tar.add(file)
+            print(file)
         tar.close()
     except IOError as e:
-        print(e.strerror)
+        print('archive error : '+ e.strerror + '\n\tfilename :' + e.filename)
 
-def mysqldump(user, password, database, file):
-    cmd = 'mysqldump -u  {0} --password={1} {2} -r {3}'.format(user, password, database, file+'.sql')
-    print('dumping database : '+database)
+def mysqldump(database, file, user, password=None):
+    if password is None:
+        cmd = 'mysqldump -u  {0} {2} -r {3}'.format(user, password, database, file + '.sql')
+    else:
+        cmd = 'mysqldump -u  {0} --password={1} {2} -r {3}'.format(user, password, database, file + '.sql')
+
+    print('dumping database : ' + database)
     status, stdout = sp.getstatusoutput(cmd)
     if status > 0:
         print("mysqldump error: " + stdout)
@@ -85,7 +90,7 @@ def main():
         make_archive(dest, src, config['compress_type'])
 
     for database in config.get('databases', []):
-        mysqldump(config['mysql_user'], config['mysql_password'], database['name'], database['file'])
+        mysqldump(database['name'], database['file'], config['mysql_user'], config['mysql_password'])
 
     if config.get('archive_all', False):
         os.chdir(config['base_dir'])
